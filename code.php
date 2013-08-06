41ef5b61f9e453768c2aa20b14f03cd6dad0cd31
Jason Schuller <hi@jason.sc>
2013-08-06 14:24:55 -0700

    jQuery in Footer

--- code.php
+++ code.php
@@ -5,7 +5,7 @@ class Code extends \Leeflets\Template\Code {
 	function setup_hooks() {
 	    $this->enqueue_style( 'style', 'css/component.css' );
 	    $this->enqueue_script( 'modernizr', 'js/modernizr.custom.js' );
-	    $this->enqueue_script( 'jquery', array(), null );
+	    $this->enqueue_script( 'jquery', '', '', '', true );
 	    $this->enqueue_script( 'boxlayout', 'js/boxlayout.js', '','1.0', true );
 	}
 }
